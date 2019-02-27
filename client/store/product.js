adf40bb4763044dbda5d74aaca5016f2e02ad7fb
kirstenlindsmith <kirstenlindsmith@gmail.com>
2019-02-27 11:09:23 -0500

    added product reducer/store functionality, added individual product API route

---
+++ client/store/product.js
@@ -0,0 +1,64 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_PRODUCT = 'GET_PRODUCT'
+const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
+
+/**
+ * INITIAL STATE
+ */
+const defaultProducts = {
+  allProducts: [],
+  selectedProduct: {}
+}
+
+/**
+ * ACTION CREATORS
+ */
+const getOneProduct = product => ({type: GET_PRODUCT, product})
+const getAllProducts = products => ({type: GET_ALL_PRODUCTS, products})
+const removeProduct = () => ({type: REMOVE_PRODUCT})
+
+/**
+ * THUNK CREATORS
+ */
+
+const fetchAllProducts = () => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get('/api/products')
+      dispatch(getAllProducts(data))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+const fetchOneProduct = productId => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get(`/api/products/${productId}`)
+      dispatch(getOneProduct(data))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultProducts, action) {
+  switch (action.type) {
+    case GET_PRODUCT:
+      return {...state, selectedProduct: action.product}
+    case GET_ALL_PRODUCTS:
+      return {...state, allProducts: action.products}
+    default:
+      return state
+  }
+}
