386d64101fc60d3d4c5ab2bf84f31ae16355fdd0
kirstenlindsmith <kirstenlindsmith@gmail.com>
2019-02-28 16:29:09 -0500

    brief commit before switching branches

--- client/store/cart.js
+++ client/store/cart.js
@@ -5,26 +5,35 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GET_CART = 'GET_CART'
+const GET_ORDER = 'GET_ORDER'
 
 /**
  * INITIAL STATE
  */
-const cart = []
+const cartState = {
+  orderItems: [],
+  order: {}
+}
 
 /**
  * ACTION CREATORS
  */
 const getCartItems = cart => ({type: GET_CART, cart})
+const getOrder = order => ({type: GET_ORDER, order})
 
 /**
  * THUNK CREATORS
  */
 
-export const fetchCartItems = () => {
+export const fetchCartItems = orderId => {
   return async dispatch => {
     try {
       const {data} = await axios.get('/api/cart')
-      dispatch(getCartItems(data))
+      const correctCart = data.filter(item =>
+        console.log(orderId, item.orderId)
+      )
+      console.log(correctCart)
+      dispatch(getCartItems(correctCart))
     } catch (error) {
       console.error(error)
     }
@@ -36,7 +45,19 @@ export const addCartItem = ids => {
   return async dispatch => {
     try {
       await axios.post('/api/cart', ids)
-      dispatch(fetchCartItems())
+
+      dispatch(fetchCartItems(ids.orderId))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export const fetchOrder = () => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get('/api/cart/unpurchased')
+      dispatch(getOrder(data))
     } catch (error) {
       console.error(error)
     }
@@ -46,10 +67,12 @@ export const addCartItem = ids => {
 /**
  * REDUCER
  */
-export default function(state = cart, action) {
+export default function(state = cartState, action) {
   switch (action.type) {
     case GET_CART:
-      return action.cart
+      return {...state, cart: action.cart}
+    case GET_ORDER:
+      return {...state, order: action.order}
     default:
       return state
   }
