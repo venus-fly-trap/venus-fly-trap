8419bf74db9d5f2c04c4c05be8958daa60d20ba5
kirstenlindsmith <kirstenlindsmith@gmail.com>
2019-03-04 12:24:00 -0500

    fixed background

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -30,7 +30,7 @@ const AuthForm = props => {
             <button type="submit">{displayName}</button>{' '}
             <button type="button" className="googleOAuth">
               <a href="/auth/google">{displayName} with Google</a>
-            </button>
+            </button>{' '}
           </div>
           {error && error.response && <div> {error.response.data} </div>}
         </form>
@@ -42,15 +42,17 @@ const AuthForm = props => {
 
         <div className="landing-page">
           <img src="https://www.bunnings.com.au/-/media/au/diy-advice-house/articles/garden/planters/how%20to%20keep%20potted%20plants%20in%20great%20condition/how%20to%20keep%20potted%20plants%20in%20great%20condition_header.jpg" />
-          <h4 className="promotion">This week only, BOGO!</h4>
-          <br />
-          <button type="button" className="btn">
-            {' '}
-            <Link to="/products" className="link">
+          <div className="promotionDiv">
+            <h4 className="promotion">This week only, BOGO!</h4>
+            <br />
+            <button type="button" className="btn">
               {' '}
-              SHOP{' '}
-            </Link>{' '}
-          </button>
+              <Link to="/products" className="link">
+                {' '}
+                SHOP{' '}
+              </Link>{' '}
+            </button>
+          </div>
         </div>
       </div>
     )
