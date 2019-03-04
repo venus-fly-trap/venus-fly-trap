71542e517f0eb40382370c50d7497e9ae48c0489
Kirsten <43189403+kirstenlindsmith@users.noreply.github.com>
2019-03-04 13:14:44 -0500

    New landing page (#93)
    * fixed background
    
    * tweaks

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -15,16 +15,22 @@ export const UserHome = props => {
       <br />
       <center>
         <div className="landing-page">
-          <img src="https://www.bunnings.com.au/-/media/au/diy-advice-house/articles/garden/planters/how%20to%20keep%20potted%20plants%20in%20great%20condition/how%20to%20keep%20potted%20plants%20in%20great%20condition_header.jpg" />
-          <h4 className="promotion">This week only, BOGO!</h4>
-          <br />
-          <button type="button" className="btn">
-            {' '}
-            <Link to="/products" className="link">
+          {/* <img src="https://i.imgur.com/jWPMHL6.gif" id="img" /> */}
+          <video preload="auto" autoPlay="autoplay" loop="loop" id="img">
+            <source src="https://i.imgur.com/jWPMHL6.mp4" type="video/mp4" />
+          </video>
+          <div id="landingDiv">
+            <h4 className="promotion" id="promotion">
+              This week only, BOGO!
+            </h4>
+            <button type="button" className="btn">
               {' '}
-              SHOP{' '}
-            </Link>{' '}
-          </button>
+              <Link to="/products" className="link">
+                {' '}
+                SHOP{' '}
+              </Link>{' '}
+            </button>
+          </div>
         </div>
       </center>
     </div>
