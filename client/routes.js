1f5da958b80aae46b8bba6af8c4f2c7b93b6d3a7
kirstenlindsmith <kirstenlindsmith@gmail.com>
2019-03-05 21:28:20 -0500

    feat(routes): fixed logged out home page

--- client/routes.js
+++ client/routes.js
@@ -31,6 +31,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/" component={UserHome} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/cart" component={Cart} />
