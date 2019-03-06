bd9f76cba07ddb0be4ee286cc15a06952e363ff5
Jamila Toaha <jtoahacoding@gmail.com>
2019-03-06 09:47:36 -0500

    Update 404page.js

--- client/components/404page.js
+++ client/components/404page.js
@@ -6,7 +6,7 @@ const NoMatch = () => {
       {/* <img src="https://i.imgur.com/fb4mWRN.png"/> */}
       <img src="https://i.imgur.com/YVPyOuL.png" />
 
-      <h3>Sorry, this page doesn't exist!</h3>
+      <h3>{"Sorry, this page doesn\'t exist\!"}</h3>
     </div>
   )
 }
