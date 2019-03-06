1db13a53ac696429feff4b439df68e82eec3bf25
Jamila Toaha <jtoahacoding@gmail.com>
2019-03-06 11:40:45 -0500

    Update index.js (#130)

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
 module.exports = router
 
+router.use('*', (req, res) => {
+  if(req.isAuthenticated()) {
+    res.send('you hit the authentication endpoint\n')
+  } else {
+    res.redirect('/nomatch')
+  }
+})
+
+
 router.use('/users', require('./users'))
 router.use('/products', require('./products'))
 router.use('/cart', require('./cart'))
