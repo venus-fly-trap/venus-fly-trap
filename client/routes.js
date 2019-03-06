678941d119db7b2e50bdcd81c9780c5a113e0445
Kirsten <43189403+kirstenlindsmith@users.noreply.github.com>
2019-03-06 10:47:36 -0500

    fixed home (#128)

--- client/routes.js
+++ client/routes.js
@@ -32,6 +32,8 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+
+
         <Route exact path="/" component={UserHome} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
